76869d7682e7f4ef93d9f4f8a12c1d5e2fa3a205
Paula Perdomo <mpaulaperdomo03@gmail.com>
2021-07-12 23:40:14 -0400

    Implemented the constructor function in model.py

--- model.py
+++ model.py
@@ -3,3 +3,12 @@ import torch.nn as nn
 import torch.optim as optim 
 import torch.nn.functional as F
 import os 
+
+class Linear_QNet(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        #the two layers of the neural network
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
