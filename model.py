6305b12b9b5668f5482c7370a6a961a4b3796e05
Paula Perdomo <mpaulaperdomo03@gmail.com>
2021-07-13 22:53:06 -0400

    Added the save function for the model file

--- model.py
+++ model.py
@@ -20,3 +20,11 @@ class Linear_QNet(nn.Module):
         x = self.linear2(x)
         return x
         
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        #save state dictionart
+        torch.save(self.state_dict(), file_name)
