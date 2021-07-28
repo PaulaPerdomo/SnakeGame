2ac5839f7e530347ff5c24d5fc530e41a0bc3a45
Paula Perdomo <mpaulaperdomo03@gmail.com>
2021-07-27 22:41:58 -0400

    Implemented plotting function

--- plotting.py
+++ plotting.py
@@ -3,4 +3,18 @@ import IPython as display
 
 plt.icon()
 
+def plot(scores, mean_scores):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    plt.title('Training AI')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.ylim(ymin=0)
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+
+
 
